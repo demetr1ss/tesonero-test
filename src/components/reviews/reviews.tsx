c476d82d88a5452965dcfe1b86baf8ca55b91d98
Dmitry T <92183078+demetr1ss@users.noreply.github.com>
2023-03-26 19:39:21 +0300

    feat: стилизует блок reviews

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -6,31 +6,37 @@ export default function Reviews(): JSX.Element {
       <h2 className={styles.title}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
       <ul className={styles.list}>
         <li className={`${styles.item} ${styles.big}`}>
-          <img src='./img/avatar.png' alt='аватар' />
-          <p className={styles.reviewText}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut consectetur animi
-            autem aliquid consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae deleniti,
-            iste nisi expedita, provident excepturi officia! Porro ab rerum omnis magnam eligendi error nobis
-            dolore? Porro ab rerum omnis magnam eligendi error nobis dolore? Porro ab rerum omnis magnam
-            eligendi error nobis dolore?
-          </p>
-          <span className={styles.reviewAuthor}>Jane Doe</span>
+          <img src='./img/avatar.png' alt='аватар' width={187} height={190} />
+          <div className={styles.quote}>
+            <p className={styles.reviewText}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut consectetur animi
+              autem aliquid consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae
+              deleniti, iste nisi expedita, provident excepturi officia! Porro ab rerum omnis magnam eligendi
+              error nobis dolore? Porro ab rerum omnis magnam eligendi error nobis dolore? Porro ab rerum
+              omnis magnam eligendi error nobis dolore?
+            </p>
+            <span className={styles.reviewAuthor}>Jane Doe</span>
+          </div>
         </li>
-        <li className={styles.item}>
-          <img src='./img/avatar.png' alt='аватар' />
-          <p className={styles.reviewText}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut consectetur animi
-            autem aliquid
-          </p>
-          <span className={styles.reviewAuthor}>Jane Doe</span>
+        <li className={`${styles.item} ${styles.left}`}>
+          <img src='./img/avatar.png' alt='аватар' width={97} height={99} />
+          <div className={styles.quote}>
+            <p className={styles.reviewText}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut
+              <br /> consectetur animi autem aliquid
+            </p>
+            <span className={styles.reviewAuthor}>Jane Doe</span>
+          </div>
         </li>
-        <li className={styles.item}>
-          <img src='./img/avatar.png' alt='аватар' />
-          <p className={styles.reviewText}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut consectetur animi
-            autem aliquid
-          </p>
-          <span className={styles.reviewAuthor}>Jane Doe</span>
+        <li className={`${styles.item} ${styles.right}`}>
+          <img src='./img/avatar.png' alt='аватар' width={97} height={99} />
+          <div className={styles.quote}>
+            <p className={styles.reviewText}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut
+              <br /> consectetur animi autem aliquid
+            </p>
+            <span className={styles.reviewAuthor}>Jane Doe</span>
+          </div>
         </li>
       </ul>
     </div>
