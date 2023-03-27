df2ce5fdc9abe4ac1a548847fa34254fe0d84d90
Dmitry T <92183078+demetr1ss@users.noreply.github.com>
2023-03-27 15:11:09 +0300

    feat: добавит стили для FAQ

--- src/components/faq/faq.tsx
+++ src/components/faq/faq.tsx
@@ -3,7 +3,7 @@ import styles from './faq.module.scss';
 export default function FAQ(): JSX.Element {
   return (
     <div className={styles.faq}>
-      <h2 className={styles.title}>Lorem ipsum, dolor sit adipisicing elit.</h2>
+      <h2 className={styles.title}>Lorem ipsum, dolor sit<br /> adipisicing elit.</h2>
       <p className={styles.description}>Porro ab rerum omnis magnam eligendi error nobis dolore?</p>
       <ul className={styles.questionsList}>
         <li className={styles.questionsItem}>
@@ -26,7 +26,7 @@ export default function FAQ(): JSX.Element {
             <span className='visually-hidden'>показать ответ</span>
           </button>
         </li>
-        <li className={styles.questionsItem}>
+        <li className={`${styles.questionsItem} ${styles.active}`}>
           <p className={styles.question}>Lorem ipsum, dolor sit amet adipisicing elit.</p>
           <p className={styles.reply}>
             Laboriosam quas, aut consectetur animi autem aliquid consequuntur suscipit exercitationem laborum
